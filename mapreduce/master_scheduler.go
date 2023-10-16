e2b748809f1186d2d5389077e452c4eaa3011dfb
BerHoffmann <ber.hoffmannsilva@gmail.com>
2023-10-16 17:55:34 -0300

    Lab2 MapReduce

---
+++ mapreduce/master_scheduler.go
@@ -0,0 +1,76 @@
+package mapreduce
+
+import (
+	"log"
+	"sync"
+)
+
+// Schedules map operations on remote workers. This will run until InputFilePathChan
+// is closed. If there is no worker available, it'll block.
+func (master *Master) schedule(task *Task, proc string, filePathChan chan string) int {
+	//////////////////////////////////
+	// YOU WANT TO MODIFY THIS CODE //
+	//////////////////////////////////
+
+	var (
+		wg        sync.WaitGroup
+		filePath  string
+		worker    *RemoteWorker
+		operation *Operation
+		counter   int
+	)
+
+	log.Printf("Scheduling %v operations\n", proc)
+
+	counter = 0
+	for filePath = range filePathChan {
+		operation = &Operation{proc, counter, filePath}
+		counter++
+
+		worker = <-master.idleWorkerChan
+		wg.Add(1)
+		go master.runOperation(worker, operation, &wg)
+	}
+	wg.Wait()
+	go master.processFailedOperations(&wg)
+	
+
+	log.Printf("%vx %v operations completed\n", counter, proc)
+	return counter
+}
+
+func (master *Master) processFailedOperations(wg *sync.WaitGroup) {
+	for elem := range master.failedOperationsChan {
+		worker := <-master.idleWorkerChan
+		wg.Add(1)
+		go master.runOperation(worker, elem, wg)
+	}
+}
+
+// runOperation start a single operation on a RemoteWorker and wait for it to return or fail.
+func (master *Master) runOperation(remoteWorker *RemoteWorker, operation *Operation, wg *sync.WaitGroup) {
+	//////////////////////////////////
+	// YOU WANT TO MODIFY THIS CODE //
+	//////////////////////////////////
+
+	var (
+		//count int
+		err  error
+		args *RunArgs
+	)
+
+	log.Printf("Running %v (ID: '%v' File: '%v' Worker: '%v')\n", operation.proc, operation.id, operation.filePath, remoteWorker.id)
+
+	args = &RunArgs{operation.id, operation.filePath}
+	err = remoteWorker.callRemoteWorker(operation.proc, args, new(struct{}))
+
+	if err != nil {
+		log.Printf("Operation %v '%v' Failed. Error: %v\n", operation.proc, operation.id, err)
+		master.failedOperationsChan <- operation
+		master.failedWorkerChan <- remoteWorker
+		wg.Done()
+	} else {
+		wg.Done()
+		master.idleWorkerChan <- remoteWorker
+	}
+}
